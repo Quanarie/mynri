cca890bd73fc5a0da70b0eb5268010bb7ee057a7
viacheslav.popov <viacheslav.popov@horus.com.pl>
2024-03-15 19:57:28 +0100

    init

---
+++ src/main/java/com/mynri/mynri/MynriApplication.java
@@ -0,0 +1,13 @@
+package com.mynri.mynri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MynriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mynri/mynri/MynriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mynri.mynri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
